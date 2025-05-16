09b714adb7e1e6eea065fa141b262ce90bf75f5c
Gokul Nath A <gokulnath0827@gmail.com>
2025-05-16 23:47:34 +0530

    refactor: update rollup and typescript configuration for improved module handling

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,40 +1,36 @@
-import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import packageJson from './package.json' with { type: 'json' }
-import dts from 'rollup-plugin-dts'
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+import pkg from "./package.json" with { type: "json" };
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
+        file: pkg.module,
         format: "esm",
         sourcemap: true,
       },
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
     ],
+    external: ["react", "react-dom"],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({
-        tsconfig: "./tsconfig.json",
-        exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts"],
-      }),
+      typescript({ tsconfig: "./tsconfig.json" }),
     ],
-    external: [...Object.keys(packageJson.peerDependencies)],
   },
   {
-    input: 'src/index.ts',
-    output: {
-      file: packageJson.types,
-      format: 'es'
-    },
-    plugins:[dts()]
-  }
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
 ];
